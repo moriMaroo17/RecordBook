05b129097e475a725768db6aa6dc7f740bb63354
Sareff <ilbezr@gmail.com>
2021-05-12 20:53:44 +0300

    Added TODO and Record Book link

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-import express from 'express'
+import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+// TODO: Connect MongoDB
 
 app.set('view engine', 'ejs');
-app.use(express.static(join(__dirname, '/views')))
+app.use(express.static(join(__dirname, '/views')));
 
 app.get('/', (req, res) => {
-  res.render('pages/index')
+  res.render('pages/index');
 })
 
 app.get('/profile', (req, res) => {
-  res.render('pages/profile')
+  res.render('pages/profile');
+})
+
+app.get('/book', (req, res) => {
+  res.render('pages/book');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
