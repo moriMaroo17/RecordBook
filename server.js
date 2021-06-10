1f4545eb68dfbb6b79a87a28b1e9a3860d66fa2b
grigory <morimaroo17@gmail.com>
2021-06-10 12:57:30 +0300

    Add records page

--- server.js
+++ server.js
@@ -15,36 +15,23 @@ const { MongoClient } = pkg;
 const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
 const url = 'mongodb://localhost:27017/'
 
-// mongoClient.connect(function (err, client) {
-
-//   if (err) {
-//     return console.log(err);
-//   } else {
-//     console.log('connection established');
-//   }
-//   // database interaction 
-//   client.close();
-// });
-
 
 app.set('view engine', 'ejs');
 app.use(express.static(join(__dirname, '/views')));
 
 app.get('/', (req, res) => {
 
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("recordbookdb");
-    // var query = { address: "Park Lane 38" };
-    dbo.collection("students").find().toArray(function(err, result) {
+
+    dbo.collection("students").find().toArray(function (err, result) {
       if (err) throw err;
       console.log(result);
-      res.render('pages/index', {students: result});
+      res.render('pages/index', { students: result });
       db.close();
     });
   });
-
-  // res.render('pages/index', {students: result});
 })
 
 app.get('/profile', (req, res) => {
@@ -64,7 +51,24 @@ app.get('/profile', (req, res) => {
 })
 
 app.get('/book', (req, res) => {
-  res.render('pages/book');
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("recordbookdb");
+    var query = { first_name: 'max', last_name: 'pokrovskiy'}
+
+    dbo.collection("students").find(query, { 
+      '_id': false,
+      'first_name': false,
+      'last_name': false,
+      'records': true, 
+    }).toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result[0]['records']['math']);
+      res.render('pages/book', { records: result[0]['records']['math'] });
+      db.close();
+    });
+  });
+  // res.render('pages/book');
 })
 
 app.listen(port, () => {
